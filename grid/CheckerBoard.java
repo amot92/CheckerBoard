db99413efb2c5bbd4edb523d9989dffde1ea9661
Adam Oakes <amot92@mail.missouri.edu>
2018-02-02 11:58:11 -0600

    Setting up local repository in parent directory

---
+++ grid/CheckerBoard.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grid;
+
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author adammoakes
+ */
+public class CheckerBoard {
+    private int numRows;
+    private int numCols;
+    private double boardWidth;
+    private double boardHeight;
+    private Color lightColor;
+    private Color darkColor;
+    
+    public CheckerBoard(int numRows, int numCols, double boardWidth, double boardHeight){
+        
+    }
+    
+    public CheckerBoard(int numRows, int numCols, double boardWidth, double boardHeight, Color lightColor, Color darkColor){
+        
+    }
+}
