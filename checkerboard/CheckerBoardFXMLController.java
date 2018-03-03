fb01bc31658437ef6d4b3ab8f5734cb57466515e
amot92 <31709233+amot92@users.noreply.github.com>
2018-03-03 14:20:28 -0600

    Testing iOS

--- checkerboard/CheckerBoardFXMLController.java
+++ checkerboard/CheckerBoardFXMLController.java
@@ -24,7 +24,7 @@ import javafx.stage.Stage;
  * FXML Controller class
  *
  * @author adammoakes
- */
+ */ — testing iOS —
 public class CheckerBoardFXMLController implements Initializable {
 
     private Stage stage;
